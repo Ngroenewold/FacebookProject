25b3fe69695c83e77cc728effda422fab596d1d0
Nolan Groenewold <nc-groenewold@wiu.edu>
2018-07-30 14:45:52 -0500

    Using selium to scrape Youtube comments off a video

--- venv/Lib/scrapper.py
+++ venv/Lib/scrapper.py
@@ -1,4 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
 
-print(2)
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.youtube.com/watch?v=66y-llDYxIs")
+driver.execute_script('window.scrollTo(1,500);')
+
+time.sleep(5)
+driver.execute_script('window.scrollTo(1,3000);')
+
+comment_div=driver.find_element_by_xpath('//*[@id="contents"]')
+comments=comment_div.find_elements_by_xpath('//*[@id="content-text"]')
+for comment in comments:
+    print(comment.text)
+
+#page = requests.get("https://www.youtube.com/watch?v=66y-llDYxIs")
+#soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
+#list(soup.children)
+#print([type(item) for item in list(soup.children)])
+#html = list(soup.children)[2]
+#print(html)
+#print(list(html.children))

e93e68d7f4644f64a3ec728b445bb8102bf36351
Ngroenewold <37720999+Ngroenewold@users.noreply.github.com>
2018-08-01 21:39:08 -0500

    Working on looping to bottom of page without infinite loop

--- venv/Lib/YouTubeAPIScrapper.py
+++ venv/Lib/YouTubeAPIScrapper.py
@@ -1,24 +1,19 @@
-from selenium import  webdriver
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from itertools import islice
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get("https://www.youtube.com/watch?v=Qn7OuGW9KEU")
-
-# get both instances of the html tag to use of the logic of the loop iterating through the page
-# while ! elm_list[1] // do code
-#elm_list = driver.find_elements_by_tag_name('</html>')
+driver.get("https://www.youtube.com/watch?v=KzKfIsTvJ4Q")
+time.sleep(2)
+driver.execute_script('window.scrollTo(1,500);')
 elm = driver.find_element_by_tag_name('html')
-elm.send_keys(Keys.END)
-time.sleep(1)
-for x in range(1,30):
+time.sleep(5)
+for x in range(1, 150):
     elm.send_keys(Keys.END)
     time.sleep(1)
-    print(x)
-comment_div= driver.find_element_by_xpath('//*[@id="contents"]')
-comments=comment_div.find_elements_by_xpath('//*[@id="content-text"]')
+comment_div = driver.find_element_by_xpath('//*[@id="contents"]')
+comments = comment_div.find_elements_by_xpath('//*[@id="content-text"]')
 for comment in comments:
     print(comment.text)
 driver.close()
